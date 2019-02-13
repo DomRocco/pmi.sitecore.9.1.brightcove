8bcd3bb953d1925a6a298328282b05a57c010b12
KUR <konstantin_razguliaev@epam.com>
2019-02-13 11:08:13 +0300

    master commit

---
+++ src/Brightcove.MediaFramework.Brightcove/Constants.cs
@@ -0,0 +1,29 @@
+﻿namespace Brightcove.MediaFramework.Brightcove
+{
+    public static class Constants
+    {
+        public static readonly string BrightCoveOauthService = "mediaframework_brightcove_oauth";
+        public static readonly string BrightCoveCmsService = "mediaframework_brightcove_cms";
+        public static readonly string BrightCoveDynamicIngestService = "mediaframework_brightcove_dynamic_ingest";
+
+        public static readonly string BrightcoveVideoCustomFieldsRouteName = "Video.Customfields";
+        public static readonly string BrightcoveVideoTextTracksRouteName = "Video.TextTracks";
+        public static readonly string BrightcoveFileDownloadRouteName = "File.Download";
+
+        public static readonly string MvcDefaultRouteName = "DefaultMvc";
+        public static readonly string WebApiDefaultRouteName = "DefaultApi";
+        public static readonly string BrightcoveIngestionCallbackRouteName = "Ingestion.Callback";
+
+        public static readonly string BrightcoveFileDownloadRouteTemplate = "/files/{fileId}";
+
+        public static readonly string BrightcoveVideoCustomFieldsRouteTemplate = "/accounts/{accountItemId}/videos/{videoId}/customfields";
+        public static readonly string BrightcoveVideoTextTracksRouteTemplate = "/accounts/{accountItemId}/videos/{videoId}/texttracks";
+
+        public static readonly string BrightcoveIngestionCallbackRouteTemplate = "/ingestioncallback/{operationId}";
+
+        public static readonly string DefaultRouteTemplate = "/{controller}/{action}";
+
+        public static readonly string SitecoreRestSharpService = "mediaframework_brightcove";
+        public static readonly string IndexName = "mediaframework_brightcove_index";
+    }
+}
---
+++ src/Brightcove.MediaFramework.Brightcove/Import/PlayListCollectionImporter.cs
@@ -0,0 +1,18 @@
+﻿using Brightcove.MediaFramework.Brightcove.Proxy.CMS;
+using RestSharp;
+using RestSharp.Authenticators;
+using PlayList = Brightcove.MediaFramework.Brightcove.Entities.PlayList;
+
+namespace Brightcove.MediaFramework.Brightcove.Import
+{
+    /// <summary>
+    /// PlayList Collection Importer
+    /// </summary>
+    public class PlayListCollectionImporter : Import.EntityCollectionImporter<PlayList>
+    {
+        public override Entities.Collections.PagedCollection<PlayList> RetrieveList(int limit, int offset, IAuthenticator authenticator)
+        {
+            return new PlaylistProxy(authenticator).RetrieveList(limit, offset);
+        }
+    }
+}
